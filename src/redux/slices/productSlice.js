93b49794e861d0e9d53efc388c7fc3ecc580eaa0
IMTISAL HUSSAIN <imtisalhussan@gmail.com>
2026-04-16 18:36:11 +0500

    reduxsetup complete

---
+++ src/redux/slices/productSlice.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// API CALL
+export const fetchProducts = createAsyncThunk(
+  "product/fetchProducts",
+  async () => {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    return data;
+  }
+);
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    products: [],
+    filteredData: [],
+    loading: false,
+  },
+  reducers: {
+    searchProduct: (state, action) => {
+      state.filteredData = state.products.filter(item =>
+        item.title.toLowerCase().includes(action.payload.toLowerCase())
+      );
+    },
+
+    filterCategory: (state, action) => {
+      if (action.payload === "all") {
+        state.filteredData = state.products;
+      } else {
+        state.filteredData = state.products.filter(item =>
+          item.category.toLowerCase().includes(action.payload)
+        );
+      }
+    },
+  },
+
+  extraReducers: builder => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.products = action.payload;
+        state.filteredData = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.loading = false;
+      });
+  },
+});
+
+export const { searchProduct, filterCategory } = productSlice.actions;
+export default productSlice.reducer;
--- src/screens/Auth/LoginScreen.jsx
+++ src/screens/Auth/LoginScreen.jsx
@@ -1,17 +1,83 @@
-import React, { useState } from 'react';
-import { View, TextInput, Text, StyleSheet, Alert } from 'react-native';
-import CustomButton from '../../components/Common/CustomButton';
+// import React, { useState } from 'react';
+// import { View, TextInput, Text, StyleSheet, Alert } from 'react-native';
+// import CustomButton from '../../components/Common/CustomButton';
+
+// const LoginScreen = ({ navigation }) => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleLogin = () => {
+//     // Hardcoded login
+//     if (email === 'admin@gmail.com' && password === '123456') {
+//       navigation.replace('Home'); // later we will add Home
+//     } else {
+//       Alert.alert('Invalid Credentials');
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Login</Text>
+
+//       <TextInput
+//         placeholder="Email"
+//         placeholderTextColor="gray"
+//         style={styles.input}
+//         onChangeText={setEmail}
+//       />
+
+//       <TextInput
+//         placeholder="Password"
+//         placeholderTextColor="gray"
+//         secureTextEntry
+//         style={styles.input}
+//         onChangeText={setPassword}
+//       />
+
+//       <CustomButton title="Login" onPress={handleLogin} />
+//     </View>
+//   );
+// };
+
+// export default LoginScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     padding: 20
+//   },
+//   title: {
+//     fontSize: 24,
+//     marginBottom: 20
+//   },
+//   input: {
+//     borderWidth: 1,
+//     marginBottom: 10,
+//     padding: 10,
+//     color:"gray"
+//   }
+// });
+
+
+import React, { useState } from "react";
+import { View, TextInput, Text, StyleSheet, Alert } from "react-native";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../../redux/slices/authSlice";
+import CustomButton from "../../components/Common/CustomButton";
 
 const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
-    // Hardcoded login
-    if (email === 'admin@gmail.com' && password === '123456') {
-      navigation.replace('Home'); // later we will add Home
+    if (email === "admin@gmail.com" && password === "123456") {
+      dispatch(loginSuccess({ email }));
+      navigation.replace("Home");
     } else {
-      Alert.alert('Invalid Credentials');
+      Alert.alert("Invalid Credentials");
     }
   };
 
@@ -19,30 +85,16 @@ const LoginScreen = ({ navigation }) => {
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
 
-      <TextInput
-        placeholder="Email"
-        placeholderTextColor="gray"
-        style={styles.input}
-        onChangeText={setEmail}
-      />
-
-      <TextInput
-        placeholder="Password"
-        placeholderTextColor="gray"
-        secureTextEntry
-        style={styles.input}
-        onChangeText={setPassword}
-      />
+      <TextInput placeholder="Email" style={styles.input} onChangeText={setEmail} />
+      <TextInput placeholder="Password" secureTextEntry style={styles.input} onChangeText={setPassword} />
 
       <CustomButton title="Login" onPress={handleLogin} />
     </View>
   );
 };
 
-export default LoginScreen;
-
 const styles = StyleSheet.create({
-  container: {
+container: {
     flex: 1,
     justifyContent: 'center',
     padding: 20
@@ -57,4 +109,9 @@ const styles = StyleSheet.create({
     padding: 10,
     color:"gray"
   }
-});
+
+})
+  
+
+
+export default LoginScreen;
